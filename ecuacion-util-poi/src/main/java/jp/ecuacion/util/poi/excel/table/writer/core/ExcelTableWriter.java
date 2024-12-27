474ef388ca9cc51e60603e9ec2fd236698b5fe5b
Yosuke Tanaka <yosuke.tanaka@ecuacion.jp>
2024-12-27 21:20:22 +0900

    Updated.

--- ecuacion-util-poi/src/main/java/jp/ecuacion/util/poi/excel/table/writer/core/ExcelTableWriter.java
+++ ecuacion-util-poi/src/main/java/jp/ecuacion/util/poi/excel/table/writer/core/ExcelTableWriter.java
@@ -1,15 +1,17 @@
 /*
  * Copyright © 2012 ecuacion.jp (info@ecuacion.jp)
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
- * in compliance with the License. You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software distributed under the License
- * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
- * or implied. See the License for the specific language governing permissions and limitations under
- * the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package jp.ecuacion.util.poi.excel.table.writer.core;
 
@@ -77,7 +79,6 @@ public abstract class ExcelTableWriter<T> extends ExcelTable<T> implements IfExc
    * Obtains header list.
    * 
    * @param tableColumnSize tableColumnSize
-   * @param dataList dataList
    * @return the list
    * @throws IOException IOException
    * @throws AppException AppException
@@ -142,11 +143,10 @@ public abstract class ExcelTableWriter<T> extends ExcelTable<T> implements IfExc
       }
     }
 
-    // 出力用のストリームを用意
-    FileOutputStream out = new FileOutputStream(destFilePath);
-
-    // ファイルへ出力
-    excel.write(out);
+    // 出力用のストリームを用意。ファイルへ出力
+    try (FileOutputStream out = new FileOutputStream(destFilePath);) {
+      excel.write(out);
+    }
   }
 
   /**
