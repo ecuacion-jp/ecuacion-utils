7b72dcfbdfdc1cb041d23f5fce8c543179e939d5
Yosuke Tanaka <yosuke.tanaka@ecuacion.jp>
2025-01-19 11:48:48 +0900

    \#25:Log messages and message in RuntimeException updated.

--- ecuacion-util-poi/src/main/java/jp/ecuacion/util/poi/excel/util/ExcelReadUtil.java
+++ ecuacion-util-poi/src/main/java/jp/ecuacion/util/poi/excel/util/ExcelReadUtil.java
@@ -1,17 +1,15 @@
 /*
  * Copyright © 2012 ecuacion.jp (info@ecuacion.jp)
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
 package jp.ecuacion.util.poi.excel.util;
 
@@ -38,7 +36,7 @@ public class ExcelReadUtil {
   private static final String EMPTY_STRING = "";
 
   private String noDataString;
-  
+
   private String dateFormat = "yyyy-MM-dd";
 
   /**
@@ -64,7 +62,7 @@ public class ExcelReadUtil {
 
     this.noDataString = (noDataString == NoDataString.EMPTY_STRING) ? EMPTY_STRING : null;
   }
-  
+
   /**
    * Sets dateFormat.
    * 
@@ -222,14 +220,15 @@ public class ExcelReadUtil {
         }
 
         if (warning) {
-          detailLog.warn("エクセル表示上の数値と実際のセルの値が異なっています。表示上：" + fmtVal + "、セルの値：" + toStrVal);
+          detailLog.warn("The number actual and displayed in excel differs. actual: " + toStrVal
+              + "、displayed: " + fmtVal);
         }
 
         return fmtVal;
       }
 
     } else {
-      throw new RuntimeException("cellの型が当てはまりません。");
+      throw new RuntimeException("cell type not found. cellType: " + cellType.toString());
     }
   }
 }
