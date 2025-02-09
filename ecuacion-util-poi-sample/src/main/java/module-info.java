185c4df131bebade72d1d018c749c8a09e72de66
Yosuke Tanaka <yosuke.tanaka@ecuacion.jp>
2025-02-09 11:32:10 +0900

    #75: now adapted to java 9 module system.

---
+++ ecuacion-util-poi-sample/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module jp.ecuacion.util.poi.sample {
+  exports jp.ecuacion.util.poi.sample.readstringfromcell;
+  exports jp.ecuacion.util.poi.sample.copytable;
+
+  requires jp.ecuacion.lib.core;
+  requires jp.ecuacion.util.poi;
+  requires org.apache.poi.poi;
+  requires org.slf4j;
+}
